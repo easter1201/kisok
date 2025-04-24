c0827a13862e386efcad4d6b73b6adac087c9fb0
easter1201 <kite1201kr@gmail.com>
2025-04-24 13:23:34 +0900

    Lv4 준비

---
+++ src/Lv4/Kiosk.java
@@ -0,0 +1,49 @@
+package Lv4;
+
+import Lv3.MenuItem;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private List<MenuItem> menuItems;
+
+    public Kiosk(List<MenuItem> menuItems){
+        this.menuItems = menuItems;
+    }
+
+    public void start(){
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("[SHAKESHACK MENU]");
+                int number = 0;
+                for (MenuItem menu : menuItems) {
+                    System.out.print(++number + ". " + menu.getName() + "| ");
+                    System.out.print(menu.getPrice() + " | ");
+                    System.out.println(menu.getDescription());
+                }
+                System.out.println("0. 종료           | 종료");
+                int input = Integer.parseInt(sc.nextLine());
+
+                if (input > 0 && input <= menuItems.size()) {
+                    MenuItem select = menuItems.get(input - 1);
+                    System.out.println("선택한 메뉴 : " + select.getName() + " | " + select.getPrice() + " | " + select.getDescription() + "\n");
+                }
+                else if (input == 0) {
+                    break;
+                } else {
+                    System.out.println("잘못된 메뉴 번호입니다.");
+                }
+            }catch(NumberFormatException e){
+                System.out.println("문자는 입력할 수 없습니다.");
+            }
+            catch(Exception e){
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+        sc.close();
+    }
+
+}
